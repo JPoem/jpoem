f955c0928e74009f2e28f1cdbe1f792397dd2b1e
jincheng.zhang <jincheng.zhang@thoughtworks.com>
2021-11-24 15:59:42 +0800

    [create] 基础框架搭建

---
+++ src/main/kotlin/cn/jpoem/plugin/actions/JpoemLogin.kt
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2021. JPoem Project.
+ */
+
+package cn.jpoem.plugin.actions
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+object JpoemLogin : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        TODO("Not yet implemented")
+    }
+}
